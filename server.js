e8bf97c76520d1b7e36979589e8e6da9f046b7cb
Alex Sherman <asherman.ca@gmail.com>
2017-10-15 21:46:54 -0700

    fe be skeletons complete

---
+++ server.js
@@ -0,0 +1,30 @@
+import config from './config';
+// import apiRouter from './api';
+import express from 'express';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+const server = express();
+server.use(bodyParser.json());
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
+
+// this will look at the views directory for an index file
+server.set('view engine', 'ejs');
+
+// import serverRender from './serverRender';
+
+server.get(['/'], (req, res) => {
+  res.render('index');
+});
+
+// server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, config.host, () => {
+  console.info('Express listening on port ', config.port);
+});
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import App from './components/core/app';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import MyAwesomeReactComponent from './components/core/app';
+
+// const App = () => (
+//   <MuiThemeProvider>
+//     <MyAwesomeReactComponent />
+//   </MuiThemeProvider>
+// );
+
+ReactDom.render(
+  <div>test</div>,
+  document.getElementById('root')
+);
